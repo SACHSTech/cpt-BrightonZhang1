7033ab4851cd6097b6ef8ed911b44e0e2c420dab
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2023-01-22 19:18:27 -0500

    update

---
+++ src/application/CSVScraper.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CSVScraper {
+
+}
--- src/cpt/README.md
+++ src/cpt/README.md
@@ -1,1 +1,2 @@
 package for your cpt.  do not put your project README details here, put it in the main project README
+link: https://ourworldindata.org/energy
