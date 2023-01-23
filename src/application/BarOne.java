f67917086ec0693c16bc694f5f211de3742f90aa
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2023-01-23 10:32:11 -0500

    update

--- src/application/BarOne.java
+++ src/application/BarOne.java
@@ -9,7 +9,6 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
@@ -19,6 +18,9 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -31,22 +33,21 @@ import javafx.stage.Stage;
 public class BarOne extends Application {
   DataExtract m = new DataExtract();
 
-  public void setCountryEnergys(HashMap<String, ArrayList<Energy>> countryEnergys) {
-    this.countryEnergys = countryEnergys;
+  public void setCountryEnergy(HashMap<String, ArrayList<Energy>> countryEnergys) {
+    this.countryEnergy = countryEnergys;
   }
 
   private BarChart chart;
   private CategoryAxis xAxis;
   private NumberAxis yAxis;
   ArrayList<String> years;
-  HashMap<String, ArrayList<Energy>> countryEnergys;
+  HashMap<String, ArrayList<Energy>> countryEnergy;
 
   public Parent createContent() throws IOException {
-    Group root = new Group();
     m.csvConvert();
-    countryEnergys = m.getcountryEnergy();
+    countryEnergy = m.getcountryEnergy();
     years = m.getYearList();
-    int n = this.years.size();
+    int n = years.size();
     String arrYears[] = new String[n];
 
     // Copying contents of s to arr[]
@@ -58,7 +59,7 @@ public class BarOne extends Application {
     yAxis = new NumberAxis("kWh / Person", 0.0d, 130000.0d, 5000.0d);
     ArrayList<BarChart.Series> series = new ArrayList<BarChart.Series>();
 
-    for (Map.Entry<String, ArrayList<Energy>> set : countryEnergys.entrySet()) {
+    for (Map.Entry<String, ArrayList<Energy>> set : countryEnergy.entrySet()) {
       String country = set.getKey();
       ArrayList<Energy> items = set.getValue();
       ArrayList<BarChart.Data> data = new ArrayList<BarChart.Data>();
@@ -100,7 +101,19 @@ public class BarOne extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    primaryStage.setScene(new Scene(createContent(), 1280, 720 ));
+    TabPane tabPane = new TabPane();
+    Tab tab1 = new Tab("Planes", new Label("Show all planes available"));
+    Tab tab2 = new Tab("Cars"  , new Label("Show all cars available"));
+    Tab tab3 = new Tab("Boats" , new Label("Show all boats available"));
+
+    tabPane.getTabs().add(tab1);
+    tabPane.getTabs().add(tab2);
+    tabPane.getTabs().add(tab3);
+
+    VBox vBox = new VBox(tabPane);
+    Scene scene = new Scene(vBox);
+
+    primaryStage.setScene(new Scene(createContent(), 800, 600 ));
     primaryStage.show();
   }
 
