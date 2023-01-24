e6f24ca6d3a08c9cb45ad0ee0f65a8ce0ae10ef5
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2023-01-24 18:11:39 -0500

    comments

--- src/application/Energy.java
+++ src/application/Energy.java
@@ -1,10 +1,24 @@
 package application;
 
+/**
+ * Energy class is for storing information on each data point
+ * 
+ * @author B. Zhang
+ */
+
 public class Energy {
+    // instance variables
     private String strCountry;
     private String intYear;
     private double dblkWh;
 
+    /**
+     * Constructor
+     * 
+     * @param country country of data point
+     * @param year year of data point
+     * @param kWh amount of energy of data point
+     */
     public Energy(String country, String year, double kWh){
         strCountry = country;
         intYear = year;
@@ -15,23 +29,11 @@ public class Energy {
         return strCountry;
     }
 
-    public void setStrCountry(String strCountry) {
-        this.strCountry = strCountry;
-    }
-
     public String getIntYear() {
         return intYear;
     }
 
-    public void setIntYear(String intYear) {
-        this.intYear = intYear;
-    }
-
     public double getDblkWh() {
         return dblkWh;
-    }
-
-    public void setDblkWh(double dblkWh) {
-        this.dblkWh = dblkWh;
     }
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,9 @@
 package application;
+/**
+ * Main program for starting program and setting the scene
+ * 
+ * @author B. Zhang
+ */
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,16 +14,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    /**
+     * Main method runs JavaFX
+     * @param args command line argument
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    /*
+     * Start method
+     */
     public void start(Stage primaryStage) throws IOException {
+        // Creates new DataExtract object
         DataExtract dataExtract = new DataExtract();
+
+        // csvConvert() extracts csv data from the DataExtract class
         dataExtract.csvConvert();
+
+        // Hashmap and arraylist to store coutnry, year, and energy information
         HashMap<String, ArrayList<Energy>> countryEnergy = dataExtract.getcountryEnergy();
         ArrayList<String> years = dataExtract.getYearList();
+
+        // Runs main program
         primaryStage.setScene(new Scene(new Charts(countryEnergy, years).createContent(), 1280, 720));
         primaryStage.show();
     }
